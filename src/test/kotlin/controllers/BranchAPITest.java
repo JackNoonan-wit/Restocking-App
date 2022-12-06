b3a32212c04db26acedc6e00242609b0b94fcc53
Jack Noonan <jackpersonal2018@gmail.com>
2022-12-06 22:44:18 +0000

    reformatted and made the display better for the user

---
+++ src/test/kotlin/controllers/BranchAPITest.java
@@ -0,0 +1,20 @@
+package controllers
+
+import models.Branch
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.Assertions.assertFalse
+import org.junit.jupiter.api.Assertions.assertNull
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+import persistence.JSONSerializer
+import persistence.XMLSerializer
+import java.io.File
+import kotlin.test.assertEquals
+
+
+
+public class BranchAPITest {
+
+}
